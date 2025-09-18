074c57075ed67a7433af78d1a33abef81d56ee0c
Aidarous Jibril <aidarousj7@gmail.com>
2025-09-18 22:18:55 +0200

    fix: prod backend entrypoint -> server.js

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,5 +1,5 @@
 import expressAsyncHandler from "express-async-handler";
-import Todo from "../models/Todo.js";
+import Todo from "../models/Todo";
 
 // GET /api/todos
 export const list = expressAsyncHandler(async (req, res) => {
